69e6d6c80bb7bdfb8b9d69908607d8dec3f4cb7f
vijayalakshmi jyoti <148619431+Vijayalakshmi558@users.noreply.github.com>
2025-05-23 15:56:39 +0530

    Add files via upload

---
+++ appgithub-main/README.md
@@ -0,0 +1,1 @@
+## This ia python app
---
+++ appgithub-main/tests/test_operation.py
@@ -0,0 +1,15 @@
+from src.math_operations import add,sub
+
+def test_add():
+    assert add(2,3)==5
+    assert add(-1,1)==0
+
+    def tst_sub():
+        assert add(5,3)==2
+        assert add(4,3)==1
+        assert add(3,3)==0
+        assert add(2,3)==-1
+
+
+
+    
